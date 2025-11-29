82dfb6ce3fbdae312cd6a732c8797c159a283dba
Daniel Gil <178396@upslp.edu.mx>
2025-11-28 23:00:59 -0600

    Se agrego nuevos archivos de prueba para condicionales, bucles y variables, y se actualizo artifacts de compilación para rutas de entorno Linux (Solo en mi caso)

--- obj/Debug/net10.0/Compilador.AssemblyInfo.cs
+++ obj/Debug/net10.0/Compilador.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Compilador")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2185afde33aa925a2c4f7060346ca40095a6daf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Compilador")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Compilador")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
